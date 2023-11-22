428f1891096602048b07a2909468d430e0b408c7
aliu19 <liu.and@northeastern.edu>
2023-11-21 20:34:40 -0500

    removed cocoapods

--- Pods/Target Support Files/Pods-MobileAppDevFinal/Pods-MobileAppDevFinal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppDevFinal : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppDevFinal
-@end
--- Pods/Target Support Files/Pods-MobileAppDevFinalTests/Pods-MobileAppDevFinalTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MobileAppDevFinalTests : NSObject
-@end
-@implementation PodsDummy_Pods_MobileAppDevFinalTests
-@end
